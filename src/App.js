f39eddf52b4fd375396f9059a038806fe197e7da
Jared Rogers <rogersjaredt@gmail.com>
2021-05-19 19:45:44 -0700

    created aggregation funcs

--- src/App.js
+++ src/App.js
@@ -4,32 +4,73 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNav from './Components/Navbar.jsx';
 import MonthlyBudgetView from './Views/MonthlyBudgetView';
 
+//------------------------------Helper Funcs / Expressions ------------------------------------//
+
 const today = new Date();
 const currentMonth = today.getMonth() + 1 + '-' + today.getFullYear();
+
 
 const reducer = (prevState, updatedProperty) => ({
   ...prevState,
   ...updatedProperty,
 });
 
-////
+function sumProperty (categoryGroup, property) {
+  const reducer = (sum, currentCategory) => {
+    return sum + currentCategory[property];
+  }
+  return categoryGroup.reduce(reducer, 0);
+};
+
+function compileSums (ObjOfObjs, property) {
+ var int = Object.keys(ObjOfObjs).reduce((sum, currentObj) => {
+    return sum + sumProperty(ObjOfObjs[currentObj], property);
+  }, 0)
+  return int;
+};
+
+function sumTransactions (transactionArr, targetCategory) {
+  const filterCategory = (transaction) => {
+      return transaction.category === targetCategory
+  }
+
+  var targetTransactions;
+  !targetCategory ? targetTransactions = transactionArr : targetTransactions = transactionArr.filter(filterCategory);
+
+
+  return sumProperty(targetTransactions, 'amount');
+};
+
+
+
+//---------------------------------------------------------------------------------------------//
+//------------------              THE  APP               --------------------------------------//
+//--------------                  ᕙ(⇀‸↼‶)ᕗ                   ----------------------------------//
+//---------------------------------------------------------------------------------------------//
+
 function App() {
+  //---------------------------- //State Management\\ ----------------------------
+  //Selected Month
   const [selectedMonth, setSelectedMonth] = useState(currentMonth);
   let monthRoot = localStorageData.months[selectedMonth];
+
+  //Selected View
+
+  //Data for selected month
   const [categories, setCategories] = useReducer(reducer, monthRoot.categories);
   const [monthData, setMonthData] = useReducer(reducer, monthRoot.data);
 
-  useEffect(() => {
-    monthRoot = localStorageData.months[selectedMonth];
-    setCategories(monthRoot.categories);
-    setMonthData(monthRoot.data);
-  }, [selectedMonth]);
+  
+  //--------------------------- //Aggregation Funcs\\ -----------------------------
+  
 
+
+  //-------------------------- //Centralized Access\\ ----------------------------
   const contextData = {
-    selectedMonth, setSelectedMonth, categories, setCategories, monthRoot
+    selectedMonth, setSelectedMonth, categories, setCategories, 
   }
-
-
+  
+  //------------------------------ //Rendering\\ ---------------------------------
   return (
     <localStorage.Provider value={contextData}>
       <div className="App">
@@ -40,6 +81,6 @@ function App() {
       </div>
     </localStorage.Provider>
   );
-}
+};
 
 export default App;
