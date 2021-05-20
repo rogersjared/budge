1fd1958e2a7af9b2a8075bfad25dbbddae0a3550
Jared Rogers <rogersjaredt@gmail.com>
2021-05-19 21:07:52 -0700

    implemented monthly aggregation funcs for categories

--- src/App.js
+++ src/App.js
@@ -10,23 +10,24 @@ const today = new Date();
 const currentMonth = today.getMonth() + 1 + '-' + today.getFullYear();
 
 
-const reducer = (prevState, updatedProperty) => ({
-  ...prevState,
-  ...updatedProperty,
-});
+function reducer (prevState, updatedProperty) {
+  return {
+    ...prevState,
+    ...updatedProperty,
+  };
+} ;
 
 function sumProperty (categoryGroup, property) {
-  const reducer = (sum, currentCategory) => {
-    return sum + currentCategory[property];
+  const propReducer = (sum, currentCategory) => {
+    return sum + Number(currentCategory[property]);
   }
-  return categoryGroup.reduce(reducer, 0);
+  return categoryGroup.reduce(propReducer, 0);
 };
 
 function compileSums (ObjOfObjs, property) {
- var int = Object.keys(ObjOfObjs).reduce((sum, currentObj) => {
+ return Object.keys(ObjOfObjs).reduce((sum, currentObj) => {
     return sum + sumProperty(ObjOfObjs[currentObj], property);
-  }, 0)
-  return int;
+  }, 0);
 };
 
 function sumTransactions (transactionArr, targetCategory) {
@@ -34,22 +35,19 @@ function sumTransactions (transactionArr, targetCategory) {
       return transaction.category === targetCategory
   }
 
-  var targetTransactions;
+  let targetTransactions;
   !targetCategory ? targetTransactions = transactionArr : targetTransactions = transactionArr.filter(filterCategory);
-
-
   return sumProperty(targetTransactions, 'amount');
 };
 
 
 
-//---------------------------------------------------------------------------------------------//
-//------------------              THE  APP               --------------------------------------//
-//--------------                  ᕙ(⇀‸↼‶)ᕗ                   ----------------------------------//
-//---------------------------------------------------------------------------------------------//
+
 
 function App() {
-  //---------------------------- //State Management\\ ----------------------------
+  //-----------------------------------------------------------------------------//
+  //    --------------------     //State Management\\     --------------------    
+  //-----------------------------------------------------------------------------//
   //Selected Month
   const [selectedMonth, setSelectedMonth] = useState(currentMonth);
   let monthRoot = localStorageData.months[selectedMonth];
@@ -59,10 +57,21 @@ function App() {
   //Data for selected month
   const [categories, setCategories] = useReducer(reducer, monthRoot.categories);
   const [monthData, setMonthData] = useReducer(reducer, monthRoot.data);
+  //-------------------------------------------------------------------------------//
+  
 
-  
-  //--------------------------- //Aggregation Funcs\\ -----------------------------
-  
+
+  //------------------------------ //Data Aggregation\\ --------------------------------
+  useEffect(()=>{
+    const budgeted = compileSums(categories, 'budgeted');
+    const funded = compileSums(categories, 'funded');
+    const spent = compileSums(categories, 'spent');
+    setMonthData({ budgeted, funded, spent })
+  }, [categories])
+
+  useEffect(()=>{
+    console.log(monthData);
+  }, [monthData]);
 
 
   //-------------------------- //Centralized Access\\ ----------------------------
@@ -70,7 +79,28 @@ function App() {
     selectedMonth, setSelectedMonth, categories, setCategories, 
   }
   
-  //------------------------------ //Rendering\\ ---------------------------------
+//-------------------------------------------------------------------------------------------------//
+//--------------------------------  RELEASE        ------------------------------------------------//
+//------------------            T H E    A P P                -----------------------------------//
+//                                 .-'   `'.
+//                                /         \
+//                                |         ;
+//                                |         |           ___.--,
+//                       _.._     |0) ~ (0) |    _.---'`__.-( (_.
+//                __.--'`_.. '.__.\    '--. \_.-' ,.--'`     `""`
+//               ( ,.--'`   ',__ /./;   ;, '.__.'`    __
+//               _`) )  .---.__.' / |   |\   \__..--""  """--.,_
+//              `---' .'.''-._.-'`_./  /\ '.  \ _.-~~~````~~~-._`-.__.'
+//                    | |  .' _.-' |  |  \  \  '.               `~---`
+//                     \ \/ .'     \  \   '. '-._)
+//                      \/ /        \  \    `=.__`~-.
+//                      / /\         `) )    / / `"".`\
+//                , _.-'.'\ \        / /    ( (     / /
+//                 `--~`   ) )    .-'.'      '.'.  | (
+//                        (/`    ( (`          ) )  '-;
+//                         `      '-;         (-'
+//---------------------------------------------------------------------------------------------//
+
   return (
     <localStorage.Provider value={contextData}>
       <div className="App">
@@ -82,5 +112,6 @@ function App() {
     </localStorage.Provider>
   );
 };
+
 
 export default App;
